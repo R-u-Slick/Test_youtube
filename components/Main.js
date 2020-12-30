6ce3ffc9f80bf1832485db86f6c1fe78e7cfa8f8
R-u-Slick <sadykovruslan91@gmail.com>
2020-12-30 23:43:14 +0300

    commit

---
+++ components/Main.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import UrlParse from './UrlParse'
+//import PropTypes from 'prop-types';
+
+import './Main.css';
+
+class Main extends React.Component {
+
+  static propTypes = {
+    //name: PropTypes.string.isRequired,
+  };
+
+  render() {
+
+    return (
+      <div className="Main">
+        <UrlParse/>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default Main;
---
+++ components/UrlParse.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+//import PropTypes from 'prop-types';
+
+import './UrlParse.css';
+
+class UrlParse extends React.Component {
+
+  static propTypes = {
+    //name: PropTypes.string.isRequired,
+  };
+
+  render() {
+
+    return (
+      <div className="Searchbar">
+        <label>Please, enter URL</label>
+        <input name='video-search' type="text" placeholder="Search..."/>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default UrlParse;
